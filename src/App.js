b9843a6edfd4201c13b26cd10307415891cd4b40
unknown <nenegrakrina@gmail.com>
2021-06-24 20:53:39 +0300

    form validation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { BrowserRouter } from 'react-router-dom'
 import Layout from './hoc/Layout/Layout.js'
 import { Route, Switch } from 'react-router-dom'
 import Quiz from './containers/Quiz/Quiz'
@@ -7,19 +6,19 @@ import QuizList from './containers/QuizList/QuizList'
 import Auth from './containers/Auth/Auth.js'
 import QuizCreator from './containers/QuizCreator/QuizCreator'
 
-function App() {
-  return (
-    <Layout>
-      <BrowserRouter>
+class App extends Component {
+  render() {
+    return (
+      <Layout>
         <Switch>
           <Route path="/auth" component={Auth} />
           <Route path="/quiz-creator" component={QuizCreator} />
           <Route path="/quiz/:id" component={Quiz} />
           <Route path="/" component={QuizList} />
         </Switch>
-      </BrowserRouter>
-    </Layout>
-  )
+      </Layout>
+    )
+  }
 }
 
 export default App
